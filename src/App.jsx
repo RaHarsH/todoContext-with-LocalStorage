07ef3b31ed40c86a0568c578cd95895fc1e8af38
Harshith R <harshith.raju0710@gmail.com>
2024-09-15 12:22:40 +0530

    completed with context and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { TodoForm, TodoItem } from "./components";
 import { TodoProvider } from "./contexts"
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
 
   const addTodo = (todo) => {
     setTodos( (prevTodos) => [{ id: Date.now(), ...todo }, ...prevTodos] )
+    // console.log(`This is a todo obj : ${todo}`)
   }
 
   const updateTodo = (id, todo) => {
@@ -21,16 +23,35 @@ function App() {
     setTodos( (prevTodos) => prevTodos.map( (prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo ) )
   }
 
+  useEffect(() => {
+    // provide the key when getting the item
+    const todos = JSON.parse(localStorage.getItem("todos")) // in local storage data is stored in the form of string so convert it to json (object form)
+    
+    if(todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, [])
+
+  // whenever there is any change in the todos array this useEffect will run and again it will set the item in the local storage
+  useEffect(() => {
+    // provide key and value when setting the item
+    localStorage.setItem("todos", JSON.stringify(todos)) // u need to store the data  in string format
+  }, [todos])
+
   return (
     <TodoProvider value={ {todos, addTodo, updateTodo, deleteTodo, toggleComplete} }>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
-              {/* Todo form goes here */} 
+              <TodoForm />
             </div>
             <div className="flex flex-wrap gap-y-3">
-              {/*Loop and Add TodoItem here */}
+              {todos.map( (todo) => (
+                <div key={todo.id} className="w-full">
+                  <TodoItem todo={todo} />
+                </div>
+              ) )}
             </div>
         </div>
       </div>
